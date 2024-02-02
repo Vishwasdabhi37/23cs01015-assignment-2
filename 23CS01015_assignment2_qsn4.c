68d42e2e0f8fbeabe149f3625b1ac0d9cdb527fb
Vishwasdabhi37 <152868443+Vishwasdabhi37@users.noreply.github.com>
2024-02-02 15:47:09 +0530

    Update 23CS01015_assignment2_qsn4.c

--- 23CS01015_assignment2_qsn4.c
+++ 23CS01015_assignment2_qsn4.c
@@ -7,13 +7,14 @@ int main()
     scanf("%d", &year);
     if ((year % 4 == 0))
     {
-        if (year % 100 == 0 && year % 400 == 0)
+        if (year % 100 != 0)
         {
             printf("year %d is a leap year.\n", year);
         }
-        else
+
+        if (year % 100 == 0 && year % 400 == 0)
         {
-            printf("year %d is not leap year.\n", year);
+            printf("year %d is a leap year.\n", year);
         }
     }
     else
